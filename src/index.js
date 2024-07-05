794a4354f450b50378deb0f97cab0969ae896849
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-05 14:21:38 -0300

    Adicionando explicacao sobre metodo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// Criando objeto que representa um player
+
 const player1 = {
     NOME: "Mario",
     VELOCIDADE: 4,
@@ -14,6 +16,11 @@ const player2 = {
     PONTOS: 0,
 }
 
-function rollDice() {
+// Simulando o lançamento de um dado
+// Math.random() Retorna um numero entre 0 e 1
+// Multiplicando por 6 faz com que retorne um numero entre 0 e 6
+// Math.floor() arredonda um numero para baixo (Inteiro) Entao o resultado seria entre 0 e 5
+// Adicionando o 1, o resultado seria entre 1 e 6 (Inteiro)
+async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
