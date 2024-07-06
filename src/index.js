90a81f3bf924766fded2148866bd93154e5020d9
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-07-06 16:14:07 -0300

    Comparando Poder

--- src/index.js
+++ src/index.js
@@ -58,19 +58,25 @@ async function playRaceEngine(character1, character2) {
             await logRollResult(player1.NOME, "manobrabilidade", diceResult1, character1.MANOBRABILIDADE);
             await logRollResult(player2.NOME, "manobrabilidade", diceResult2, character2.MANOBRABILIDADE);
         } else if (block === "CONFRONTO") {
-            totalTestSkill1 = diceResult1 + character1.PODER;
-            totalTestSkill2 = diceResult2 + character2.PODER;
+            let powerResult1 = diceResult1 + character1.PODER;
+            let powerResult2 = diceResult2 + character2.PODER;
+
+            console.log(`${character1.NOME} confrontou ${character2.NOME}! 🥊`);
 
             await logRollResult(player1.NOME, "poder", diceResult1, character1.PODER);
             await logRollResult(player2.NOME, "poder", diceResult2, character2.PODER);
+
+            // Comparando poder
+            character2.PONTOS -= (powerResult1 > powerResult2 && character2.PONTOS > 0 ? 1 : 0);
+            character1.PONTOS -= (powerResult2 > powerResult1 && character1.PONTOS > 0 ? 1 : 0);
+
+            if (powerResult1 === powerResult2) console.log("Confronto empatado! Nenhum ponto foi perdido");
         }
 
         if (totalTestSkill1 > totalTestSkill2) {
             console.log(`${character1.NOME} marcou um ponto`);
             character1.PONTOS++;
-        } else if (totalTestSkill1 === totalTestSkill2) {
-            console.log("Empate");
-        } else {
+        } else if (totalTestSkill2 > totalTestSkill1) {
             console.log(`${character2.NOME} marcou um ponto`);
             character2.PONTOS++;
         }
